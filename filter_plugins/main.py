23000eb379e6165e46cca39be871de4e22c1c727
Michael Drogalis <mjd3089@rit.edu>
2016-03-09 19:40:14 -0800

    Add a fn for finding the private IP

--- filter_plugins/main.py
+++ filter_plugins/main.py
@@ -4,10 +4,20 @@ def natural_index_of (coll, x):
   '''
   return coll.index(x) + 1
 
+def unroll_address(x):
+  if type(x) is list or type(x) is tuple:
+    return x[0]['private_ip_address']
+  else:
+    return x['private_ip_address']
+
+def private_addresses(coll):
+  return map(unroll_address, coll)
+
 class FilterModule(object):
     ''' Ansible Jinja2 filters '''
 
     def filters(self):
       return {
         'natural_index_of': natural_index_of,
+        'private_addresses': private_addresses,
       }
